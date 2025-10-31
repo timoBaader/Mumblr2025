d775b82fb73259c5e45990d64b52129bb006d241
Timo Baader <timo_baader@hotmail.de>
2025-10-31 14:23:40 +0200

    style(identity): convert HealthController to file-scoped namespace

--- Mumblr.Identity.Api/Controllers/HealthController.cs
+++ Mumblr.Identity.Api/Controllers/HealthController.cs
@@ -1,20 +1,19 @@
 ﻿using System.Reflection;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Mumblr.Identity.Api.Controllers
-{
-    [ApiController]
-    [Route("[controller]")]
-    public class HealthController : ControllerBase
-    {
-        [HttpGet("")]
-        public IActionResult Get() => Ok(new { status = "ok", service = "identity" });
+namespace Mumblr.Identity.Api.Controllers;
 
-        [HttpGet("version")]
-        public IActionResult Version()
-        {
-            var ver = Assembly.GetExecutingAssembly().GetName().Version?.ToString() ?? "0.0.0";
-            return Ok(new { service = "identity", version = ver });
-        }
+[ApiController]
+[Route("[controller]")]
+public class HealthController : ControllerBase
+{
+    [HttpGet("")]
+    public IActionResult Get() => Ok(new { status = "ok", service = "identity" });
+
+    [HttpGet("version")]
+    public IActionResult Version()
+    {
+        var ver = Assembly.GetExecutingAssembly().GetName().Version?.ToString() ?? "0.0.0";
+        return Ok(new { service = "identity", version = ver });
     }
 }
