653b025b4b1d1a52eb92bffda4c3551be8f35dd2
Timo Baader <timo_baader@hotmail.de>
2025-10-31 14:28:57 +0200

    feat(friendship): scaffold Web API and add /health + /health/version

---
+++ Mumblr.Friendship.Api/Controllers/HealthController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+
+namespace Mumblr.Friendship.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class HealthController : ControllerBase
+{
+    [HttpGet("")]
+    public IActionResult Get() => Ok(new { status = "ok", service = "friendship" });
+
+    [HttpGet("version")]
+    public IActionResult Version()
+    {
+        var ver = Assembly.GetExecutingAssembly().GetName().Version?.ToString() ?? "0.0.0";
+        return Ok(new { service = "friendship", version = ver });
+    }
