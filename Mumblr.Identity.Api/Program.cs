c7cb4f66da25c944c9dc4c8c372c6ba2d86f37cb
Timo Baader <timo_baader@hotmail.de>
2025-10-31 17:43:14 +0200

    feat(identity): add Application/Domain/Infrastructure layers + shared libs and wire references

--- Mumblr.Identity.Api/Program.cs
+++ Mumblr.Identity.Api/Program.cs
@@ -1,13 +1,18 @@
+using Mumblr.Abstractions.Users;
+using Mumblr.Identity.Infrastructure.Users;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+builder.Services.AddSingleton<IUserRepository, InMemoryUserRepository>();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
