2a480fdb5782daf6197e4b9cbc5f19d7f950c99c
Timo Baader <57661987+timoBaader@users.noreply.github.com>
2025-10-31 13:55:02 +0200

    feat(identity): scaffold Web API and add /health + /version endpoints (#1)

---
+++ Mumblr.Identity.Api/Controllers/HealthController.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mumblr.Identity.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet("")]
+        public IActionResult Get() => Ok(new { status = "ok", service = "identity" });
+
+        [HttpGet("version")]
+        public IActionResult Version()
+        {
+            var ver = Assembly.GetExecutingAssembly().GetName().Version?.ToString() ?? "0.0.0";
+            return Ok(new { service = "identity", version = ver });
+        }
+    }
+}
