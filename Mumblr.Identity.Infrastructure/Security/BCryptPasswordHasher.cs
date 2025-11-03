a01dab6faa5130a6b244f28a92c843536688306d
Timo Baader <timo_baader@hotmail.de>
2025-11-03 17:40:58 +0200

    feat(identity): add temporary /debug/hash endpoint to validate password hashing

---
+++ Mumblr.Identity.Infrastructure/Security/BCryptPasswordHasher.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mumblr.Abstractions.Security;
+
+namespace Mumblr.Identity.Infrastructure.Security;
+
+public sealed class BCryptPasswordHasher : IPasswordHasher
+{
+    private const int WorkFactor = 12;
+
+    public string Hash(string password)
+    {
+        return BCrypt.Net.BCrypt.HashPassword(password, workFactor: WorkFactor);
+    }
+
+    public bool Verify(string password, string hash)
+    {
+        return BCrypt.Net.BCrypt.Verify(password, hash);
+    }
+}
